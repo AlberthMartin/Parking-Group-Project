cb8121dfa081cd0af9560d87556cf7d81b7e1ef1
Alberth Martin <alberth.martin01@gmail.com>
2025-10-14 19:14:16 +0300

    Revert "Moved backend files into separate folder"
    This reverts commit 88fde5e2b035d34fa5bc1e878b973a3fd8b8073c.

---
+++ src/main/java/com/example/demo/model/Vehicle.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String model;
+    private String color;
+    private String plate;
+    private int length_cm;
+    private int width_cm;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    private User createdBy;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
