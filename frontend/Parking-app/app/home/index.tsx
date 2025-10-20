6830a00ccf7400e03cc97fcda4faba413cf2733d
MNummila <maxnummila@gmail.com>
2025-10-20 12:03:14 +0300

    Changed the default templates to a simple login and home

---
+++ frontend/Parking-app/app/home/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Welcome to Parking App 🅿️</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, alignItems: "center", justifyContent: "center" },
+    text: { fontSize: 20, fontWeight: "bold" },
+});
